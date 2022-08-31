1648e3f6701a7b5081195bcd1fd63d493a0675e3
Mewansiri Jayalath <mej@tiqri.com>
2022-08-31 14:17:26 +0530

    modified the message

--- EManager-TestApp/Startup.cs
+++ EManager-TestApp/Startup.cs
@@ -32,7 +32,7 @@ namespace EManager_TestApp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World ^^^^^^^^ !");
                 });
             });
         }
